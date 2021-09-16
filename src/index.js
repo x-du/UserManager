3fec3feaedab8462d6fbeddd61bf889dc4b3d074
Harry Du <xdu@microstrategy.com>
2021-09-16 12:24:40 -0400

    upgrading the app to use create-react-app v4.0.3

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import reportWebVitals from './reportWebVitals';
 import store from './store.js'
 
-const render = () => ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const render = () => ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
 render();
 store.subscribe(render);
