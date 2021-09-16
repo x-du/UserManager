68150b4cd0e55041026aa0392c6a0be944a120bd
Harry Du <xdu@microstrategy.com>
2021-09-16 00:36:44 -0400

    fixing security warnings, adding script to deploy to ghpages

--- src/store.js
+++ src/store.js
@@ -18,9 +18,9 @@ const reducer = function (state, action) {
     }
 }
 
-const DEFAULT_USERNAME = 'tester_qfan';
-const DEFAULT_PASSWORD = 'test';
-const DEFAULT_REST_SERVER = 'http://localhost:8081/web-dossier-11.0.0.11522/api/';
+const DEFAULT_USERNAME = '';
+const DEFAULT_PASSWORD = '';
+const DEFAULT_REST_SERVER = 'https://env-nnnnnn.customer.cloud.microstrategy.com/MicroStrategyLibrary/api/';
 const initState = {users: null, username: DEFAULT_USERNAME, password: DEFAULT_PASSWORD, url: DEFAULT_REST_SERVER, loginToken: null};
 
 const store = createStore(reducer, initState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
